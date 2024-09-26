428bc9e8ac7a2802cdde59753f5156c09b1fafa3
Bruno Mesquita dos Santos <45662184+BrunoMesquitaa@users.noreply.github.com>
2024-09-26 15:40:06 -0300

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -2,5 +2,4 @@ if (!require('rsconnect'))install.packages("rsconnect");library(rsconnect)
 
 rsconnect::setAccountInfo(name='bruno-mesquita', token='1BEBD53D250AC8D6CD73B6AD5702D91A', secret='+oPQ3XzaMdMaL8wXrFQZhH0IT24g9RLU7QtQZXlI')
 
-rsconnect::terminateApp(appName = "dashboard_temperatura")
-rsconnect::deployApp(appName = "dashboard_temperatura", forceUpdate = TRUE)
+rsconnect::deployApp(forceUpdate = TRUE, appFiles = c("temperaturas.rds", "dash.qmd"))
