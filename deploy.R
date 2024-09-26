0e20b0348b981a40f8be53bafc0c33732e57bbc5
BrunoMesquitaa <onurb2378@gmail.com>
2024-09-26 15:28:03 -0300

    teste 5

--- deploy.R
+++ deploy.R
@@ -2,4 +2,5 @@ if (!require('rsconnect'))install.packages("rsconnect");library(rsconnect)
 
 rsconnect::setAccountInfo(name='bruno-mesquita', token='1BEBD53D250AC8D6CD73B6AD5702D91A', secret='+oPQ3XzaMdMaL8wXrFQZhH0IT24g9RLU7QtQZXlI')
 
-deployApp(forceUpdate = TRUE)
+rsconnect::terminateApp(appName = "dashboard_temperatura")
+rsconnect::deployApp(appName = "dashboard_temperatura", forceUpdate = TRUE)
