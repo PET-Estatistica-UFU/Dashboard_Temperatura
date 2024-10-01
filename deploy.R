38e016a59aa17868e158dd95d114bb3a1da76fc6
Bruno Mesquita dos Santos <45662184+BrunoMesquitaa@users.noreply.github.com>
2024-10-01 17:48:11 -0300

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,5 +1,7 @@
 if (!require('rsconnect'))install.packages("rsconnect");library(rsconnect)
 
-rsconnect::setAccountInfo(name='bruno-mesquita', token='1BEBD53D250AC8D6CD73B6AD5702D91A', secret='+oPQ3XzaMdMaL8wXrFQZhH0IT24g9RLU7QtQZXlI')
+rsconnect::setAccountInfo(name=Sys.getenv("NAME"), 
+                          token=Sys.getenv("TOKEN"), 
+                          secret=Sys.getenv("SECRET"))
 
 rsconnect::deployApp(forceUpdate = TRUE)
